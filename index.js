c83fdbc8e5b81eadfe7568e949d6e81a124807e2
Jonathan Bryant <spectrum19tech@gmail.com>
2021-12-23 22:12:59 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Desiree", "Hunter", "Logan"];
+
+function writeCards(names, event) {
+    const notes = [];
+
+    for (let i = 0; i < names.length; i++) {
+        notes.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return notes;
+}
+
+function countDown(num) {
+    for (let i = num; i > -1; i--) {
+      console.log(i);
+    }
+  }
